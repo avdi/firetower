d5d3ec7e80b59757e590c06e9b807f13ab869458
Dave Rogers <david.t.rogers@gmail.com>
2010-08-14 13:56:41 -0400

    display the room name in the growl notification so as to not lose context

--- lib/firetower/plugins/core/growl.rb
+++ lib/firetower/plugins/core/growl.rb
@@ -8,31 +8,34 @@ module Firetower
       end
 
       def join(session, room)
-        notify("Firetower", "Joined room \"#{room.name}\"")
+        notify("Firetower", "Joined room \"#{room.name}\"", room.name)
+        # notify("Firetower", "Joined room \\"#{room.name}\\"")
       end
 
       def leave(session, room)
-        notify("Firetower", "Left room \"#{room.name}\"")
+        # notify("Firetower", "Left room \\"#{room.name}\\"")
+        notify("Firetower", "Left room \"#{room.name}\"", room.name)
       end
 
       def receive(session, event)
         case event['type']
         when "TextMessage"
           user = event.room.account.users[event['user_id']]
-          notify(user['name'], event['body'])
+          notify(user['name'], event['body'], event.room.name)
         else
           # NOOP
         end
       end
 
       def error(session, error)
-        notify("Campfire Error", error.message)
+        notify("Campfire Error", error.message) #, 'Error')
       end
 
       private
 
       def notify(*args)
-        system('growlnotify', '-t', args[0], 
+        # system 'growlnotify' ,'-m', args.last
+        system('growlnotify', '-t', (args[2].nil? ? args[0] : "#{args[0]} - #{args[2]}"), 
           '-m', args[1], 
           '--image', File.expand_path('campfire-logo-for-fluid.png', File.dirname(__FILE__))) \
             or raise "Growl notification failed"
