060208a1a5795bccd6f71e0ea1879c02b860beec
Josh Nichols <josh@technicalpickles.com>
2011-07-06 19:35:02 -0400

    Fix compatibility with older versions. At some point, Session#join was renamed to Session#join_room. This introduces an alias back to it.

--- lib/firetower/rooms.rb
+++ lib/firetower/rooms.rb
@@ -1,9 +1,15 @@
 module Firetower
   module Rooms
+    def self.included(other)
+      
+    end
     fattr(:subscribed_rooms) { [] }
 
     def join_room(subdomain, room_name)
       subscribed_rooms << find_room(subdomain, room_name)
     end
+
+    # Fix backwards compatability with 0.0.3
+    alias_method :join, :join_room
   end
 end
