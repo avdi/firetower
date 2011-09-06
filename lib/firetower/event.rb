85d5b9bb4a77f01af8910f2c81aeaa4dfa88c69f
Josh Nichols <josh@technicalpickles.com>
2011-09-06 00:03:09 -0400

    Event improvements. Actual attributes, helpers to determine type (ie text?), and added =~ operator.

--- lib/firetower/event.rb
+++ lib/firetower/event.rb
@@ -2,6 +2,7 @@ module Firetower
   class Event
     attr_accessor :attributes
     attr_accessor :room
+    attr_accessor :created_at, :body, :id, :user_id, :type
 
     def self.parse(json)
       new(JSON.parse(json))
@@ -9,6 +10,9 @@ module Firetower
 
     def initialize(attributes)
       @attributes = attributes
+      @attributes.each do |key, value|
+        send("#{key}=", value) if respond_to?("#{key}=")
+      end
     end
 
     def [](key)
@@ -18,5 +22,22 @@ module Firetower
     def []=(key, value)
       @attributes[key] = value
     end
+
+    def text?
+      self.type == 'TextMessage'
+    end
+
+    def sound?
+      self.type == 'SoundMessage'
+    end
+
+    def timestamp?
+      self.type == 'TimestampMessage'
+    end
+
+    def =~(pattern)
+      self.body =~ pattern
+    end
+
   end
 end
